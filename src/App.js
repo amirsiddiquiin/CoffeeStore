68bc88838e67c396a9c7705bc288f238766156cc
Amir Siddiqui <amir.siddiqui@planetspark.in>
2023-03-17 01:23:47 +0530

    integrate contract

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import abi from "./contract/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Buy from "./components/Buy";
+import Memos from "./components/Memos";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0x90560bc805a02c186af7e44e5b00a386a1ee1343";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+          );
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
+      <img src="https://cdn3d.iconscout.com/3d/premium/thumb/coffee-cup-6558022-5429717.png" className="img-fluid" alt=".." width="6%" />
+      <p
+        class="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <div className="text-center" style={{ fontSize: 16, fontWeight: 600, color: 'black' }}>Connected Account No- {account}</div>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+        <Memos state={state} />
+      </div>
     </div>
   );
 }
